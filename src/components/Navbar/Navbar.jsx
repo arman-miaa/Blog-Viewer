856576431d0fd8100ec9b2fc978498223313e11d
Arman <mdarman365506@gmail.com>
2025-01-24 16:47:49 +0600

    modified profile page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,36 +6,49 @@ import {
   LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/components";
 import Link from "next/link";
+
 const Navbar = () => {
-    const { user, isAuth } = UserData();
-    console.log(user);
+  const { user, isAuth } = UserData();
+  console.log(user);
+
   return (
-    <div className="sticky top-0 z-10 bg-gray-300">
-      <div className="flex container mx-auto gap-10 justify-between items-center  p-4">
-        <div>
-          <h4 className="text-xl font-bold">
-            ✍️ <span>BlogNest</span>{" "}
+    <div className="sticky top-0 z-10 bg-gray-600 text-white shadow-md">
+      <div className="container mx-auto flex justify-between items-center p-4">
+        {/* Logo */}
+        <div className="flex items-center gap-2">
+          <h4 className="text-2xl font-bold">
+            ✍️ <span>BlogNest</span>
           </h4>
         </div>
-        <div className="flex gap-4 text-xl font-semibold">
-          <Link href={"/"} className="hover:text-green-500">
+
+        {/* Navbar Links & Auth Buttons */}
+        <div className="flex gap-6 items-center">
+          <Link
+            href={"/"}
+            className="text-lg font-semibold hover:text-green-500 transition-all duration-200"
+          >
             Home
           </Link>
-          <Link href={"/profile"} className="hover:text-green-500">
+          <Link
+            href={"/profile"}
+            className="text-lg font-semibold hover:text-green-500 transition-all duration-200"
+          >
             Profile
           </Link>
-          <div>
+
+          {/* Conditional rendering of auth buttons */}
+          <div className="flex gap-4 items-center">
             {user ? (
-              <LogoutLink className="bg-blue-400 px-4 py-1 text-white">
+              <LogoutLink className="mr-4 border border-green-500 rounded-lg px-6 py-2 hover:bg-green-500 hover:text-white transition duration-300">
                 Log Out
               </LogoutLink>
             ) : (
               <>
-                <LoginLink className="mr-3 border border-green-500 rounded hover:bg-green-500 hover:text-white px-4 py-1">
-                  Sign in
+                <LoginLink className="mr-4 border border-green-500 rounded-lg px-6 py-2 hover:bg-green-500 hover:text-white transition duration-300">
+                  Sign In
                 </LoginLink>
-                <RegisterLink className="border-green-500 border rounded hover:bg-green-500 hover:text-white  px-4 py-1">
-                  Sign up
+                <RegisterLink className="border-green-500 border rounded-lg px-6 py-2 hover:bg-green-500 hover:text-white transition duration-300">
+                  Sign Up
                 </RegisterLink>
               </>
             )}
