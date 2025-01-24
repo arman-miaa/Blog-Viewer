a05a0e7f405f08df17c2fad3f8db2aaa57756263
Arman <mdarman365506@gmail.com>
2025-01-24 11:54:24 +0600

    show all blogs post on home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
+import Blogs from "@/components/Blogs";
 import Navbar from "@/components/Navbar/Navbar";
 
 
 const page = () => {
   return (
     <div>
-     <Navbar/>
+      <Navbar />
+      <Blogs/>
     </div>
   );
 };
