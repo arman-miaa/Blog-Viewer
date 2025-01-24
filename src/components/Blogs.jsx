791bfb9d8b5cb2a157062c7915f2bf0e88137593
Arman <mdarman365506@gmail.com>
2025-01-24 15:41:04 +0600

    modified blogs card

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { useEffect, useState } from "react";
-
+import { FaIdBadge, FaUser } from "react-icons/fa";
 const Blogs = () => {
     const [blogs, setBlogs] = useState([]);
 
@@ -13,32 +13,55 @@ const Blogs = () => {
     }, []);
 
     return (
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 my-20">
-        {blogs.length > 0 &&
-          blogs.map((blog) => (
-            <div key={blog.id} className="card bg-base-100  shadow-xl ">
-              <figure>
-                <p>{blog.body}</p>
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">Title: {blog.title}</h2>
-                <p>Blog Id: {blog.id}</p>
-                <p>blog userID: {blog.userId}</p>
-                <div className="card-actions justify-end">
+      <div className="container mx-auto ">
+        <div className="mt-12 lg:mt-16 text-center">
+          <h2 className="text-xl lg:text-2xl font-bold">
+            BlogNest – Discover and Explore Blogs Effortlessly
+          </h2>
+          <p className="w-full md:w-1/2 text-center mx-auto">
+            BlogNest makes browsing and reading blogs simple, secure, and
+            seamless. Explore detailed posts with ease!
+          </p>
+        </div>
+        <div className=" grid grid-cols-1 md:grid-cols-3 gap-5 my-20">
+          {blogs.length > 0 &&
+            blogs.map((blog) => (
+              <div
+                key={blog.id}
+                className="card bg-base-100 flex flex-col shadow-xl p-6 "
+              >
+                <h2 className="card-title text-2xl font-bold flex ">
+                  {blog.title}
+                </h2>
+                <figure className="mt-2">
+                  <p>{blog.body}</p>
+                </figure>
+                <div className="card-body mt-2 flex mb-6">
+                  <p className="flex-1 flex items-center gap-2">
+                    {" "}
+                    <FaIdBadge /> Blog Id: {blog.id}
+                  </p>
+                  <p className="flex-1 flex items-center gap-2">
+                    {" "}
+                    <FaUser /> blog userID: {blog.userId}
+                  </p>
+                </div>
+                <div className="card-actions justify-end mt-auto">
                   <Link
                     href={`/blog/${blog.id}`}
-                    className="px-4 py-1 bg-gray-600 text-white"
+                    className="px-4 py-2 rounded-sm bg-gray-700 hover:bg-gray-500 text-white"
                   >
-                    Details
+                    
+                    🔍 Details
                   </Link>
                 </div>
               </div>
-            </div>
 
-            // <Link key={blog.id} href={`/blog/${blog.id}`}>
-            //   {blog.title}
-            // </Link>
-          ))}
+              // <Link key={blog.id} href={`/blog/${blog.id}`}>
+              //   {blog.title}
+              // </Link>
+            ))}
+        </div>
       </div>
     );
 };
